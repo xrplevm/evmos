0049035aac134c68aaf48f3b20e6ec91da12c02b
tom <tomasguerraalda@hotmail.com>
2023-10-12 14:58:48 -0300

    add comment

--- tests/nix_tests/test_patches.py
+++ tests/nix_tests/test_patches.py
@@ -98,6 +98,7 @@ def test_send_funds_to_distr_mod_eth_tx(evmos):
     assert receipt.status == 0  # failed status expected
 
     wait_for_new_blocks(cli, 2)
+    # only fees should be deducted from sender balance
     fees = receipt["gasUsed"] * receipt["effectiveGasPrice"]
     new_src_balance = cli.balance(eth_to_bech32(sender))
     assert old_src_balance - fees == new_src_balance
