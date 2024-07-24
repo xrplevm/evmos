84567acd6ba71e51d1050003c610dcbd5864a59d
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-07-24 14:57:51 +0200

    x/evm/types: replace `ethtypes.Message` with `core.Message`

--- x/evm/types/msg.go
+++ x/evm/types/msg.go
@@ -302,7 +302,8 @@ func (msg MsgEthereumTx) AsTransaction() *ethtypes.Transaction {
 
 // AsMessage creates an Ethereum core.Message from the msg fields
 func (msg MsgEthereumTx) AsMessage(signer ethtypes.Signer, baseFee *big.Int) (core.Message, error) {
-	return msg.AsTransaction().AsMessage(signer, baseFee)
+	message, err := core.TransactionToMessage(msg.AsTransaction(), signer, baseFee)
+	return *message, err
 }
 
 // GetSender extracts the sender address from the signature values using the latest signer for the given chainID.
