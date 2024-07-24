229bafc899a7244b7763a1dd85e478fa0edae7c6
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-07-24 14:57:13 +0200

    x/evm/keeper: replace `ethtypes.Message` with `core.Message`

--- x/evm/keeper/state_transition_test.go
+++ x/evm/keeper/state_transition_test.go
@@ -436,7 +436,7 @@ func (suite *KeeperTestSuite) TestRefundGas() {
 
 			vmdb.AddRefund(params.TxGas)
 
-			if tc.leftoverGas > m.Gas() {
+			if tc.leftoverGas > m.GasLimit {
 				return
 			}
 
@@ -444,7 +444,7 @@ func (suite *KeeperTestSuite) TestRefundGas() {
 				tc.malleate()
 			}
 
-			gasUsed := m.Gas() - tc.leftoverGas
+			gasUsed := m.GasLimit - tc.leftoverGas
 			refund := keeper.GasToRefund(vmdb.GetRefund(), gasUsed, tc.refundQuotient)
 			suite.Require().Equal(tc.expGasRefund, refund)
 
@@ -708,7 +708,7 @@ func (suite *KeeperTestSuite) TestApplyMessageWithConfig() {
 func (suite *KeeperTestSuite) createContractGethMsg(nonce uint64, signer ethtypes.Signer, cfg *params.ChainConfig, gasPrice *big.Int) (core.Message, error) {
 	ethMsg, err := suite.createContractMsgTx(nonce, signer, gasPrice)
 	if err != nil {
-		return nil, err
+		return core.Message{}, err
 	}
 
 	msgSigner := ethtypes.MakeSigner(cfg, big.NewInt(suite.ctx.BlockHeight()))
